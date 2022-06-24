026e0deced5a452e1448b3d25d2a0cd92598e67d
Sirgourny Compton <sirgournycompton@Sirgournys-iMac.local>
2022-06-24 10:06:53 -0500

    Created logging and security aspects, need to implement

---
+++ src/main/java/dev/megashopper/common/utils/aspects/SecurityAspect.java
@@ -0,0 +1,14 @@
+package dev.megashopper.common.utils.aspects;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SecurityAspect {
+
+    // TODO: Need to implement (watch recording 6.24.22, the first half
+
+
+}
