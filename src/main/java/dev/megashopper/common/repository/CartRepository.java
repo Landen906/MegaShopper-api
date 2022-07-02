4d11dd2f65a4fb1bd1b2f4f2e481637c3ba2f1be
Sirgourny Compton <sirgournyc@outlook.com>
2022-07-01 20:03:59 -0500

    Implemented Cart: Service, Controller, and Response. Updated CartRepository

--- src/main/java/dev/megashopper/common/repository/CartRepository.java
+++ src/main/java/dev/megashopper/common/repository/CartRepository.java
@@ -1,12 +1,20 @@
 package dev.megashopper.common.repository;
 
 import dev.megashopper.common.entities.Cart;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CartRepository  extends JpaRepository<Cart, String> {
+public interface CartRepository  extends JpaRepository<Cart,String> {
 
     boolean existsByItemId(String itemId);
     boolean existsByCustomerId(String customerId);
+
+    @Query("SELECT * FROM Items WHERE customerId = :?")
+    List<Cart> findItemsByCustomerId(int customerId);
+
 }
